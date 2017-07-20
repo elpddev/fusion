c852c956e11a03344832aeb3351edc4760068b55
Eyal Lapid <eyal.lapid@protonmail.com>
2017-07-20 15:22:16 +0300

    initial skeleton

---
+++ lib/fusion.ex
@@ -0,0 +1,18 @@
+defmodule Fusion do
+  @moduledoc """
+  Documentation for Fusion.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Fusion.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/fusion_test.exs
@@ -0,0 +1,8 @@
+defmodule FusionTest do
+  use ExUnit.Case
+  doctest Fusion
+
+  test "the truth" do
+    assert 1 + 1 == 2
+  end
+end
